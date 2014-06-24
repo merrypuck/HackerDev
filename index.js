dc590ea69946e2b649b544e54e3b09961f5cdc99
ihiram <joe@hiramsoftware.com>
2014-06-24 15:58:51 +0300

    Added template engine

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.engine('html', require('ejs').renderFile);
+app.engine('ejs', require('ejs-locals'));//.renderFile);
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-app.set('view options', {layout: false});
+app.set('view engine', 'ejs');
+app.set('view options', {layout: "template.html"});
 
 app.get('/', function(req, res) {
   res.render('index');
