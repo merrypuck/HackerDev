92c7b07dca8fe30070759a58c3bc806ee7c90532
ihiram <joe@hiramsoftware.com>
2014-06-25 15:26:22 +0300

    someone did something somehwere in some file

--- public/js/parking.js
+++ public/js/parking.js
@@ -1,17 +1,23 @@
 var goBtn = document.getElementById('goBtn');
 
 goBtn.addEventListener('click', function() { 
-	
+	map.setCenter(new google.maps.LatLng(32.0777415,34.7810515));
 
-	
+	var lat = map.getCenter().lat();
+	var lng = map.getCenter().lng();
 
+	console.log("There is an event listener " + map.getCenter());
 
+	var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
 
-
-
-
-
-
+          var response = JSON.parse(xhr.responseText);
+          console.log(response);
+        }
+    }
+    xhr.open("GET", 'api/do-parkour?lat='+lat+'&lon='+lng ,true);
+    xhr.send();
 
 
 /* do stuff here*/ }, false);
