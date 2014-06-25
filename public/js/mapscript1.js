af124b0b9f346e147bd695fa35abf4e87e45a19d
Aaron Landy <aaron@thecompassmag.com>
2014-06-25 15:49:10 +0300

    marker color

--- public/js/mapscript1.js
+++ public/js/mapscript1.js
@@ -19,7 +19,7 @@ function initialize() {
   // var myLatlng = new google.maps.LatLng(37.790234970864, -122.39031314844);
   var mapOptions = {
       center: new google.maps.LatLng(34.793978899999956, 32.06997550635381),
-      zoom: 16,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
   };
@@ -121,11 +121,34 @@ google.maps.event.addDomListener(window, 'load', initialize);
     });
      */
 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 
 var findMyLocation = document.getElementById('findMyLocation');
 if(navigator.geolocation) {
     browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position) {
+      console.log('asdsda');
+      console.log(position);
       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
       map.setCenter(initialLocation);
     }, function() {
--- public/js/parking.js
+++ public/js/parking.js
@@ -1,3 +1,29 @@
+var timeDistance = document.getElementById('timeDistance');
+var meterDistance = document.getElementById('meterDistance');
+
+function transitionUI() {
+  var goBtn = document.getElementById('goBtn');
+  var targetedLocation = document.getElementById('targetedLocation');
+  var markerLocation = document.getElementById('markerLocation');
+  goBtn.style.display = 'none';
+  targetedLocation.style.display = 'none';
+  markerLocation.style.display = 'none';
+
+  var foundBtn = document.getElementById('foundBtn');
+  var distanceFromDestination = document.getElementById('distanceFromDestination');
+  
+  foundBtn.style.display = 'block';
+  distanceFromDestination.style.display = 'block';
+
+}
+function setTimeAndDistance(time, meters) {
+  timeDistance.style.display = 'block';
+  timeDistance.innerHTML = time;
+  meterDistance.style.display = 'block';
+  meterDistance.innerHTML = meters;
+}
+
+
 var goBtn = document.getElementById('goBtn');
 
 goBtn.addEventListener('click', function() { 
@@ -10,6 +36,8 @@ goBtn.addEventListener('click', function() {
 
 	var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
+        transitionUI();
+
         if (xhr.readyState == 4) {
 
           var response = JSON.parse(xhr.responseText);
