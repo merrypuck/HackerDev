fde327508cfd2e61d6c3ecc3cd7ccdc9de09072b
Julia F <juliafeldmar@gmail.com>
2014-06-25 04:31:19 -0700

    cycle fixes

--- public/js/markers.js
+++ public/js/markers.js
@@ -16,6 +16,15 @@ var carImage = {
     anchor: new google.maps.Point(12.5, 12.5)
   };
 
+var arrowImage = {
+    url: '../images/arrow.png',
+	scaledSize: new google.maps.Size(25, 25),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(12.5, 12.5)
+  };
+
 function getPricedMarker(map, title, price, position) {
   return new MarkerWithLabel({
 	  	icon: spotImage,
@@ -33,5 +42,13 @@ function getCarMarker(map, position) {
 	    position: position, 
 	    map: map,
 	    icon: carImage
+	  });
+}
+
+function getContinuationMarker(map, position) {
+	return new google.maps.Marker({
+	    position: position, 
+	    map: map,
+	    icon: arrowImage
 	  });
 }
